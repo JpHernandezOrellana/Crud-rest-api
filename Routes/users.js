55c0677163cd12e0bb88025ac7be66ca450028b2
JpHernandezOrellana <ps.juanhernandez@gmail.com>
2023-09-19 15:07:46 -0300

    api inicial

---
+++ Routes/users.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import {
+  createUser,
+  getUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/users.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+
+router.post("/", createUser);
+
+// /users/2 => req.params { id:""}
+
+router.get("/:id", getUser);
+
+router.delete("/:id", deleteUser);
+
+router.patch("/:id", updateUser);
+
+export default router;
